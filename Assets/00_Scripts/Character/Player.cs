f458b5eb976ad499ae7148c3f4ec431af32dab9f
Xenawn <codeparadoxist@gmail.com>
2025-04-07 19:39:26 +0900

    [Feat] InputAction + CharacterController

--- Assets/00_Scripts/Character/Player.cs
+++ Assets/00_Scripts/Character/Player.cs
@@ -2,4 +2,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Player 입력 and 여러 컴포넌트 관리
+  [field:Header("Animations")]
+   [field:SerializeField] public PlayerAnimationData AnimationData {  get; private set; }
+
+    public Animator animator;
+    public PlayerController Input { get; private set; } 
+    public CharacterController Controller { get; private set; }
+
+    private void Awake()
+    {
+        AnimationData.Initialize();
+        animator = GetComponent<Animator>();
+        Input = GetComponent<PlayerController>();
+        Controller = GetComponent<CharacterController>();
+    }
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;   // 커서 숨기기
+    }
 }
--- Assets/00_Scripts/Character/PlayerController.cs
+++ Assets/00_Scripts/Character/PlayerController.cs
@@ -2,4 +2,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public PlayerInputs playerInputs { get; private set; }
+    public PlayerInputs.PlayerActions playerActions { get; private set; }
+
+    private void Start()
+    {
+        playerInputs = new PlayerInputs();
+        playerActions = playerInputs.Player;
+    }
+
+    private void OnEnable()
+    {
+        playerInputs.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInputs.Disable();
+    }
 }
