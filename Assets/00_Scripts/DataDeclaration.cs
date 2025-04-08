db2917924720cbe646584ece6190d15351b3d886
Jaewon <dhwodnjs0827@naver.com>
2025-04-08 19:53:50 +0900

    [Feat] ObjectPoolManager 추가
    # ObjectPoolManager.cs
    - IPoolable 인터페이스를 통한 ObjectPooling 기능 추가

--- Assets/00_Scripts/DataDeclaration.cs
+++ Assets/00_Scripts/DataDeclaration.cs
@@ -1,3 +1,8 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace DataDeclaration
 {
     #region Enum
@@ -10,8 +15,35 @@ namespace DataDeclaration
     #endregion
     
     #region Struct
+    [Serializable]
+    public struct RewardCard
+    {
+        public Image rewardImage;
+        public TextMeshProUGUI rewardName;
+        public TextMeshProUGUI timeCost;
+        public Button rewardButton;
+
+        public void SendData(RewardCard rewardData)
+        {
+            
+        }
+    }
     #endregion
     
     #region Interface
+    /// <summary>
+    /// ObjectPooling을 위한 Interface
+    /// </summary>
+    public interface IPoolable
+    {
+        /// <summary>
+        /// 리소스의 InstanceID
+        /// </summary>
+        public int ResourceInstanceID { get; set; }
+        /// <summary>
+        /// 리소스의 Prefab
+        /// </summary>
+        public GameObject GameObject { get; }
+    }
     #endregion
 }
