213649e5b32c17ca41a9da157b4234c16452c7cb
Xenawn <codeparadoxist@gmail.com>
2025-04-09 15:00:24 +0900

    [Feat] Attack Data 설정

--- Assets/00_Scripts/SO/PlayerData.cs
+++ Assets/00_Scripts/SO/PlayerData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
@@ -14,9 +15,37 @@ public class PlayerGroundData
 
 
 }
+
+[Serializable]
+public class PlayerAttackData
+{
+    [field: SerializeField] public List<AttackInfoData> AttackInfoDatas { get; private set; }
+    public int GetAttackInfoCount() { return AttackInfoDatas.Count; }  // To Do: 해보고 필요없으면 지우기
+
+    public AttackInfoData GetAttackInfoData(int index) { return AttackInfoDatas[index]; }
+
+}
+[Serializable]
+public class AttackInfoData
+{
+    [field: SerializeField] public string AttackName { get; private set; }
+    [field: SerializeField] public int ComboStateIndex { get; private set; }    // 콤보 
+    [field: SerializeField][field: Range(0f, 1f)] public float ComboTransitionTime { get; private set; }  // 콤보 시간 범위
+
+    [field: SerializeField][field: Range(0f, 3f)] public float ForceTransitionTime { get; private set; }  // 얼마만큼 힘을 줄건지? To DO: 해보고 필요없으면 지울듯
+
+    [field: SerializeField][field: Range(-10f, 10f)] public float Force { get; private set; } // 힘
+
+    [field: SerializeField] public int Damage;
+
+    [field: SerializeField] public float Dealing_Start_TransitionTime { get; private set; }
+    [field: SerializeField] public float Dealing_End_TransitionTime { get; private set; }
+
+
+}
 [CreateAssetMenu(fileName = "Player", menuName = "SO/PlayerData")]
 public class PlayerData : ScriptableObject
 {
     [field: SerializeField] public PlayerGroundData GroundData { get; private set; }
-
+    [field: SerializeField] public PlayerAttackData AttackData { get; private set; }
 }
