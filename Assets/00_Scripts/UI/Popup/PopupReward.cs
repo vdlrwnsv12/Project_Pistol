0d18c24d0cfd9c9682dcf15d10da7de59c55e4f1
Jaewon <dhwodnjs0827@naver.com>
2025-04-17 12:19:17 +0900

    [Feat] 무기 아이템 선택 기능 추가
    # HUDUI.cs
    - UI 스탯 업데이트 오버로드 메서드 추가
    
    # PopupReward.cs
    - 아이템 선택 시 적용 대상에 따라 스탯 증가 기능 추가
    - 일단 주석 처리(병합 시 주석 해제 필요)

--- Assets/00_Scripts/UI/Popup/PopupReward.cs
+++ Assets/00_Scripts/UI/Popup/PopupReward.cs
@@ -40,14 +40,23 @@ public class PopupReward : PopupUI
         //     rewardCards[i].rewardName.text = itemRewards[i].Name;
         //     rewardCards[i].timeCost.text = $"-{itemRewards[i].cost:N2}s";
         //     rewardCards[i].rewardButton.onClick.RemoveAllListeners();
-        //     if (itemRewards[i].ApplicationTarget == (int)ItemApplyType.Player)
+        //     
+        //     var playerItem = itemRewards[i];
+        //     if (itemRewards[i].ApplicationTarget == (int)ItemApplyType.Player) // 플레이어 적용 아이템
         //     {
-        //         var playerItem = itemRewards[i];
-        //         rewardCards[i].rewardButton.onClick.AddListener(() => TestManager.Instance.StatHandler.IncreaseStat(playerItem.RCL, playerItem.HDL, playerItem.STP, playerItem.SPD));
+        //         rewardCards[i].rewardButton.onClick.AddListener(() =>
+        //             TestManager.Instance.StatHandler.IncreaseStat(playerItem.RCL, playerItem.HDL, playerItem.STP,
+        //                 playerItem.SPD));
         //         rewardCards[i].rewardButton.onClick.AddListener(() =>
         //             ((HUDUI)UIManager.Instance.CurMainUI).UpdateStatValue(TestManager.Instance.StatHandler.Stat));
-        //         rewardCards[i].rewardButton.onClick.AddListener(CloseUI);
         //     }
+        //     else if (itemRewards[i].ApplicationTarget == (int)ItemApplyType.Weapon) // 무기 적용 아이템
+        //     {
+        //         rewardCards[i].rewardButton.onClick.AddListener(() =>
+        //             StageManager.Instance.Player.PlayerEquipment.WeaponStatHandler.ApplyItemStats(playerItem));
+        //     }
+        //
+        //     rewardCards[i].rewardButton.onClick.AddListener(CloseUI);
         // }
     }
 }
