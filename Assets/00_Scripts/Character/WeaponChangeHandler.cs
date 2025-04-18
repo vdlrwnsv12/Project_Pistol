bbcc5c7bc842f9809176df501ddcf2c71c5d264f
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-04-18 13:02:25 +0900

    [Refactor] 무기 생성 로직 변경

--- Assets/00_Scripts/Character/WeaponChangeHandler.cs
+++ Assets/00_Scripts/Character/WeaponChangeHandler.cs
@@ -9,28 +9,26 @@ public class WeaponChangeHandler : MonoBehaviour
     [SerializeField] private GameObject currentWeapon;
     void Update()
     {
-        Ray ray = playerEquipment.playerCam.ScreenPointToRay(Input.mousePosition);
-        
-        if (Physics.Raycast(ray, out RaycastHit hit, 100f) && hit.collider.CompareTag("Gun"))
+        // Ray ray = playerEquipment.playerCam.ScreenPointToRay(Input.mousePosition);
+
+        // if (Physics.Raycast(ray, out RaycastHit hit, 100f) && hit.collider.CompareTag("Gun"))
+        // {
+        //     WeaponSelector selector = hit.collider.GetComponent<WeaponSelector>();
+
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            WeaponSelector selector = hit.collider.GetComponent<WeaponSelector>();
+            // if (selector != null)
+            // {
+            //     // 기존 무기 다시 바닥에 보여줌
+            //     if (currentWeapon != null)
+            //         currentWeapon.SetActive(true);
 
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                if (selector != null)
-                {
-                    // 기존 무기 다시 바닥에 보여줌
-                    if (currentWeapon != null)
-                        currentWeapon.SetActive(true);
+            //     // 새 무기 들고, 인덱스로 무기 변경
+            //     currentWeapon = selector.gameObject;
+            playerEquipment.SwitchWeapon();
 
-                    // 새 무기 들고, 인덱스로 무기 변경
-                    currentWeapon = selector.gameObject;
-                    playerEquipment.SwitchWeapon(selector.weaponIndex);
-
-                    // 현재 바닥 무기 숨김 (플레이어 손으로 간다고 가정)
-                    selector.gameObject.SetActive(false);
-                }
-            }
+            // // 현재 바닥 무기 숨김 (플레이어 손으로 간다고 가정)
+            // selector.gameObject.SetActive(false);
         }
     }
 }
