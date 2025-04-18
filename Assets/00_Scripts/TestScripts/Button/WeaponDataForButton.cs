9c80ff5fd26e1f84802d9059cab5be7e931f9886
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-04-18 19:19:16 +0900

    [Feat] 총 버튼 마우스 올리면 툴팁 추가

--- Assets/00_Scripts/TestScripts/Button/WeaponDataForButton.cs
+++ Assets/00_Scripts/TestScripts/Button/WeaponDataForButton.cs
@@ -1,12 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class WeaponDataForButton : MonoBehaviour
+public class WeaponDataForButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public WeaponSO WeaponData;
+    public GameObject tooltipUI;
+    public Text tooltipText;
+
 
     public void OnClickWeaponButtton()
     {
         Debug.Log($"[버튼 클릭] WeaponData: {WeaponData.name}, ID: {WeaponData.ID}");
         GameManager.Instance.selectedWeapon = WeaponData;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (WeaponData != null && tooltipUI != null && tooltipText != null)
+        {
+            tooltipUI.SetActive(true);
+            tooltipText.text =
+                $"<b>{WeaponData.Name}</b>\n" +
+                $"ID: {WeaponData.ID}\n" +
+                $"설명: {WeaponData.Description}\n" +
+                $"공격력: {WeaponData.DMG}\n" +
+                $"반동: {WeaponData.ShootRecoil}\n" +
+                $"장전 시간: {WeaponData.ReloadTime}s\n" +
+                $"최대 탄약: {WeaponData.MaxAmmo}\n" +
+                $"가격: {WeaponData.Cost}";
+        }
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (tooltipUI != null)
+            tooltipUI.SetActive(false);
     }
 }
