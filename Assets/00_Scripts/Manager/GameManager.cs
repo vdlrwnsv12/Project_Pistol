ebca37aa29c1ef27f266643ea4676c261eb668e2
Jaewon <dhwodnjs0827@naver.com>
2025-04-21 15:09:21 +0900

    [Refactor] 유저 관련 변수 위치 이동
    # GameManager.cs
     유저 관련 필드 및 메서드 제거
    
    # UGSManager.cs
    - 유저 관련 필드 및 메서드 추가

--- Assets/00_Scripts/Manager/GameManager.cs
+++ Assets/00_Scripts/Manager/GameManager.cs
@@ -1,37 +1,7 @@
-using Unity.Services.Authentication;
-using UnityEngine;
-
 public sealed class GameManager : SingletonBehaviour<GameManager>
 {
     public CharacterSO selectedCharacter;
     public WeaponSO selectedWeapon;
-    
-    private string accessToken;
-    private string playerID;
-    private string playerName;
-    
-    public string AccessToken => accessToken;
-    public string PlayerID => playerID;
-    public string PlayerName => playerName;
-
-    private void Start()
-    {
-        AuthenticationService.Instance.SignOut();
-        SetPlayerData();
-    }
-
-    private void SetPlayerData()
-    {
-        accessToken = AuthenticationService.Instance.AccessToken;
-        playerID = AuthenticationService.Instance.PlayerId;
-        playerName = AuthenticationService.Instance.PlayerName;
-    }
-
-    public void LogOut()
-    {
-        AuthenticationService.Instance.SignOut();
-        SetPlayerData();
-    }
     
     public static void GameQuit()
     {
