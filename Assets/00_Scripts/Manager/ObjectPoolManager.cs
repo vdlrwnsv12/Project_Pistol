9fac184576ca1ce7d288e196aecbf94aa1e242e3
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-04-25 11:42:52 +0900

    [Refactor] 오브젝트 풀 매니저 완성 + 총 발사 시 탄피, 탄흔 오브젝트 풀

--- Assets/00_Scripts/Manager/ObjectPoolManager.cs
+++ Assets/00_Scripts/Manager/ObjectPoolManager.cs
@@ -1,65 +1,91 @@
 using System.Collections.Generic;
+using System.Collections;
 using DataDeclaration;
 using UnityEngine;
 
-//TODO: 구현 중, 사용X
+/// <summary>
+/// 사용법
+/// 1. `GetObjectInPool`로 풀에서 오브젝트 꺼내옴
+///    - `IPoolable` 인터페이스 있는 애들만 관리됨
+///    - 위치 회전 부모 설정 가능
+/// 2. `ReturnObjectToPool`로 객체 풀에 반환함
+///    - 반환된 객체는 비활성화되고 나중에 재사용됨
+/// 3. `AutoReturnToPool`은 일정 시간 후 자동으로 객체 반환해줌
+///    - 정해진 시간 지나면 자동 반환됨
+///
+/// 예시
+/// 1. 객체 꺼내는 법
+///    GameObject pooledObject = ObjectPoolManager.Instance.GetObjectInPool(resource position rotation);
+///
+/// 2. 객체 반환하는 법
+///    ObjectPoolManager.Instance.ReturnObjectToPool(pooledObject);
+///    ObjectPoolManager.Instance.AutoReturnToPool(pooledObject, impactPoolable.ReturnTime);
+/// </summary>
+
 public sealed class ObjectPoolManager : SingletonBehaviour<ObjectPoolManager>
 {
-    private readonly Dictionary<int, Queue<GameObject>>
-        pools = new(); // key: 리소스 InstanceID  value: IPoolable 상속받은 비활성화된 오브젝트
+    private readonly Dictionary<int, Queue<GameObject>> pools = new();
 
-    /// <summary>
-    /// 오브젝트 풀에서 오브젝트 활성화
-    /// </summary>
-    /// <param name="resource">활성화할 리소스</param>
-    /// <param name="position">생성 위치</param>
-    /// <param name="rotation">생성 방향</param>
-    /// <param name="parent">부모 Transform, 기본: null</param>
-    /// <returns></returns>
-    public GameObject GetObjectInPool(IPoolable resource, Vector3 position, Quaternion rotation,
-        Transform parent = null)
+    public GameObject GetObjectInPool(IPoolable resource, Vector3 position, Quaternion rotation, Transform parent = null)
     {
-        GameObject newObj; // 인스턴스화된 게임 오브젝트
+        GameObject instance;
+        var key = resource.ResourceInstanceID;
 
-        var key = resource.ResourceInstanceID; // 리소스의 고유 InstanceID를 key값으로
         if (pools.TryGetValue(key, out var queue) && queue.Count > 0)
         {
-            // Pool에 오브젝트 있을 경우 해당 오브젝트 Dequeue 후 활성화
-            newObj = queue.Dequeue();
-            newObj.gameObject.SetActive(true);
+            instance = queue.Dequeue();
         }
         else
         {
-            // Pool에 오브젝트 없을 경우 resource를 Instantiate
-            newObj = Instantiate(resource.GameObject);
+            instance = Instantiate(resource.GameObject);
         }
 
-        // 자식 오브젝트 여부에 따른 Transform 설정
-        if (parent)
+        instance.transform.SetParent(parent ? parent : null);
+        instance.transform.SetPositionAndRotation(position, rotation);
+        instance.SetActive(true);
+
+        // 인스턴스에서 Poolable 컴포넌트 꺼내서 적용
+        var poolable = instance.GetComponent<IPoolable>();
+        if (poolable != null && poolable.IsAutoReturn)
         {
-            newObj.transform.SetParent(parent);
+            StartCoroutine(AutoReturn(instance, poolable.ReturnTime));
         }
 
-        newObj.transform.SetPositionAndRotation(position, rotation);
-
-        return newObj;
+        return instance;
     }
 
-    /// <summary>
-    /// 오브젝트 풀에 비활성화된 오브젝트 반환
-    /// </summary>
-    /// <param name="returnObject">비활성화 오브젝트</param>
-    public void ReturnObjectToPool(IPoolable returnObject)
+    private IEnumerator AutoReturn(GameObject obj, float delay)
     {
-        var key = returnObject.ResourceInstanceID;
+        yield return new WaitForSeconds(delay);
+        if (obj != null && obj.activeInHierarchy)
+        {
+            ReturnObjectToPool(obj);
+        }
+    }
+
+    public void ReturnObjectToPool(GameObject instance)
+    {
+        var poolable = instance.GetComponent<IPoolable>();
+        if (poolable == null)
+        {
+            Debug.LogWarning($"[ObjectPoolManager] 반환 실패: IPoolable이 없음");
+            return;
+        }
+
+        int key = poolable.ResourceInstanceID;
         if (!pools.TryGetValue(key, out var queue))
         {
             queue = new Queue<GameObject>();
-            pools.Add(key, queue);
+            pools[key] = queue;
         }
 
-        queue.Enqueue(returnObject.GameObject);
-        returnObject.GameObject.SetActive(false);
-        //returnObject.GameObject.transform.SetParent(transform);
+        instance.SetActive(false);
+        instance.transform.SetParent(transform);
+        queue.Enqueue(instance);
+    }
+
+    public void AutoReturnToPool(GameObject obj, float delay)
+    {
+        StartCoroutine(AutoReturn(obj, delay));
     }
 }
