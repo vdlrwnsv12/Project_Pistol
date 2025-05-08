edd6b428e0734d63980ed020cf2aa4e832bb080c
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-05-08 18:46:05 +0900

    [Design] 로비씬 변경

--- Assets/00_Scripts/UI/Main/LobbyBtn/StartBtn.cs
+++ Assets/00_Scripts/UI/Main/LobbyBtn/StartBtn.cs
@@ -5,13 +5,20 @@ using TMPro;
 
 public class StartBtn : MonoBehaviour
 {
+    public GameObject infoUI;
     // Start is called before the first frame update
+
+    void Start()
+    {
+        infoUI.SetActive(false);
+    }
 
     [SerializeField] private GameObject canvas;
   
     public void OffLobbyUI()
     {
         canvas.SetActive(false);
+        infoUI.SetActive(true);
     }
     public void OnLobbyUI()
     {
