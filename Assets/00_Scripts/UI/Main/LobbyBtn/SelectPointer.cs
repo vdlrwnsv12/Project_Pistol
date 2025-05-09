03196d8b7caabbcc3107c62258bd9d5dbc8a0242
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-05-09 12:01:59 +0900

    [Refactor] 캐릭터 클릭처리 로직 분리 및 정리
    [Feat] back버튼으로 처음 위치로

--- Assets/00_Scripts/UI/Main/LobbyBtn/SelectPointer.cs
+++ Assets/00_Scripts/UI/Main/LobbyBtn/SelectPointer.cs
@@ -4,7 +4,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class SelectPointer : MonoBehaviour
+
+public interface ICameraReturnable
+{
+    void CamMove(Vector3 targetPos, Quaternion targetRot);
+    void CamReturn();
+}
+public class SelectPointer : MonoBehaviour, ICameraReturnable
 {
     //public GameObject popupInformPrefab; // 프리팹 연결해줘야 함
     //public Transform popupParent; // 보통 Canvas 같은 곳 (선택)
@@ -13,6 +19,7 @@ public class SelectPointer : MonoBehaviour
     private Quaternion originCamRot;
     private CinemachineVirtualCamera vcam;
 
+    private SelectSO clickComp;
     private void Awake()
     {
         inputActions = new PlayerInputs();
@@ -44,12 +51,7 @@ public class SelectPointer : MonoBehaviour
 
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            if (PopupInform.LastInstance != null)
-            {
-                UIManager.Instance.ClosePopUpUI();
-            }
-
-            SelectSO clickComp = hit.collider.GetComponent<SelectSO>();
+            clickComp = hit.collider.GetComponent<SelectSO>();
             if (clickComp != null)
             {
                 GameManager.Instance.selectedCharacter = clickComp.Data;
@@ -57,50 +59,90 @@ public class SelectPointer : MonoBehaviour
 
                 if (hit.collider.CompareTag("Player"))
                 {
+                    UIManager.Instance.ClosePopUpUI();
+                    
                     Transform target = hit.collider.transform;
 
                     Vector3 cameraPosition = target.position + target.forward * 3f + Vector3.up * 3f;
+                    Quaternion cameraRotation = Quaternion.LookRotation(target.position - cameraPosition);
 
                     //CinemachineVirtualCamera vcam = GetComponent<CinemachineVirtualCamera>();
                     if (vcam != null)
                     {
                         vcam.Follow = null;
-                        vcam.LookAt = target;
+                        vcam.LookAt = null;
 
-                        vcam.transform.position = cameraPosition;
-                        vcam.transform.LookAt(target);
+                        // vcam.transform.position = cameraPosition;
+                        // vcam.transform.LookAt(target);
+                        
+                        CamMove(cameraPosition, cameraRotation);
+                        
                     }
-                }
-                else
-                {
-                    CamReturn();
                 }
 
                 //GameObject popupGO = Instantiate(popupInformPrefab, popupParent);
 
                 UIManager.Instance.OpenPopUpUI("PopupInform");
-                PopupInform popup = PopupInform.LastInstance;
-                popup.SetCharacterInfo(clickComp.Data);
+                StartCoroutine(SetPopupInfoDelayed(clickComp.Data));
 
                 Debug.Log("직접 팝업 인스턴스 생성 완료!");
-            }
-            else
-            {
-                CamReturn();
             }
         }
     }
 
-    private void CamReturn()
+    private IEnumerator SetPopupInfoDelayed(CharacterSO data)
     {
+        yield return null;
+        PopupInform popup = PopupInform.LastInstance;
+        popup.SetCharacterInfo(data);
+        popup.SetCamReturnTarget(this);
+    }
+    
+    public void CamReturn()
+    {
+        Debug.Log("[SelectPointer] CamReturn 호출됨");
         if (vcam != null)
         {
             vcam.Follow = null;
             vcam.LookAt = null;
+        
+            CamMove(originCamPos, originCamRot);
 
-            vcam.transform.position = originCamPos;
-            vcam.transform.rotation = originCamRot;
+            // vcam.transform.position = originCamPos;
+            // vcam.transform.rotation = originCamRot;
         }
     }
 
+    public void CamMove(Vector3 targetPos, Quaternion targetRot)
+    {
+        StopAllCoroutines();
+        StartCoroutine(CamMoveCoroutine(targetPos, targetRot));
+    }
+    public IEnumerator CamMoveCoroutine(Vector3 targetPos, Quaternion targetRot)
+    {
+        float duration = 0.8f;
+        float elapsed = 0f;
+
+        Vector3 startCamPos = vcam.transform.position;
+        Quaternion startCamRot = vcam.transform.rotation;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / duration);
+
+            float easedT = EaseInOutQuad(t);
+
+            vcam.transform.position = Vector3.Lerp(startCamPos, targetPos, easedT);
+            vcam.transform.rotation = Quaternion.Slerp(startCamRot, targetRot, easedT);
+            yield return null;
+        }
+
+        vcam.transform.position = targetPos;
+        vcam.transform.rotation = targetRot;
+    }
+    private float EaseInOutQuad(float t)
+    {
+        return t < 0.5f ? 2f * t * t : -1f + (4f - 2f * t) * t;
+    }
 }
--- Assets/00_Scripts/UI/Popup/PopupInform.cs
+++ Assets/00_Scripts/UI/Popup/PopupInform.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 
 public class PopupInform : PopupUI
 {
+    private ICameraReturnable camReturnTarget;
+
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI descriptionText;
     public TextMeshProUGUI statusText;
@@ -14,6 +16,8 @@ public class PopupInform : PopupUI
     private void Awake()
     {
         LastInstance = this;
+        backButton.onClick.AddListener(OnBackButtonClicked);
+        selectButton.onClick.AddListener(OnSelectButtonClicked);
     }
 
     public void SetCharacterInfo(CharacterSO data)
@@ -24,6 +28,21 @@ public class PopupInform : PopupUI
         statusText.text = $"Status\n" +
                           $"RCL: {data.RCL.ToString().PadRight(5)} HDL: {data.HDL}\n" +
                           $"STP: {data.STP.ToString().PadRight(5)} SPD: {data.SPD}";
+    }
+
+    public void SetCamReturnTarget(ICameraReturnable target)
+    {
+        camReturnTarget = target;
+    }
+    private void OnBackButtonClicked()
+    {
+        camReturnTarget?.CamReturn();
+        UIManager.Instance.ClosePopUpUI();
+    }
+
+    private void OnSelectButtonClicked()
+    {
+        //ToDo: 선택버튼 클릭시 게임매니저에 캐릭터 데이터 보내고 총기 선택으로 카메라 이동
     }
 
     public override bool IsDestroy { get; set; }
