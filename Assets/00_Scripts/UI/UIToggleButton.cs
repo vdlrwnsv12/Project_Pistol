81e93be353cb34ef2aa8b407798c5257850eda26
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-05-12 10:27:30 +0900

    [Feat] UI토글 기능 로비 캐릭터 랜덤 애니메이션 출력기능 추가

---
+++ Assets/00_Scripts/UI/UIToggleButton.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIToggleButton : MonoBehaviour
+{
+    [SerializeField] private string popupUIName; // 토글할 팝업 UI 이름 (예: "SettingPopupUI")
+
+    private static string currentUIName = null;
+    private static bool isAnyUIOpen = false;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClickToggleUI);
+    }
+
+    private void OnClickToggleUI()
+    {
+        // 현재 열린 팝업과 같은 이름이면 닫기
+        if (isAnyUIOpen && currentUIName == popupUIName)
+        {
+            // UIManager.Instance.ClosePopUpUI();
+            // currentUIName = null;
+            // isAnyUIOpen = false;
+            return;
+        }
+        // 다른 팝업이 열려 있으면 기존 팝업 닫기
+        if (isAnyUIOpen && currentUIName != popupUIName)
+        {
+            UIManager.Instance.ClosePopUpUI();
+        }
+
+        // 새 팝업 열기
+        UIManager.Instance.OpenPopupUI<PopupOption>();
+
+        currentUIName = popupUIName;
+        isAnyUIOpen = true;
+    }
+}
+
