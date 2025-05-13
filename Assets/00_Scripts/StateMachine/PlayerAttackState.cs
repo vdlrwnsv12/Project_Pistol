8c548b38c9d8a6229132533239490c41b7d83f2c
Xenawn <codeparadoxist@gmail.com>
2025-05-13 12:50:00 +0900

    [Fix] 공격시 연발 수정

--- Assets/00_Scripts/StateMachine/PlayerAttackState.cs
+++ Assets/00_Scripts/StateMachine/PlayerAttackState.cs
@@ -27,13 +27,12 @@ public class PlayerAttackState : PlayerBaseState
         
         var animInfo = stateMachine.Player.Animator.GetCurrentAnimatorStateInfo(0);
 
-        // 애니메이션이 일정 진행률에 도달하면 한 번만 총알 발사
-        if (!hasShot && animInfo.IsName("Attack") && animInfo.normalizedTime > 0)
+        if (!hasShot && animInfo.IsName("Attack") && animInfo.normalizedTime >= 0.3f && animInfo.normalizedTime < 0.9f)
         {
             hasShot = true;
             Shoot();
         }
-        
+
         // 애니메이션 종료 시 Idle 상태로 전환
         if (animInfo.IsName("Attack") && animInfo.normalizedTime >= 1)
         {
