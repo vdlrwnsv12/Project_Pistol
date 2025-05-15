6d63ab40ae67f296a60df9c2321fb76438f332cb
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-05-15 12:11:02 +0900

    [Feat] 총알 충돌 사운드 구분
    #BulletImpactSound.cs
    -콜라이더 태그 별 다른 사운드 출력

--- Assets/00_Scripts/Object/Sound/BulletImpactSound.cs
+++ Assets/00_Scripts/Object/Sound/BulletImpactSound.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class BulletImpactSound : MonoBehaviour
 {
     //TODO: 재질 별로 소리 다르게
-    [SerializeField]private AudioClip[] bulletImpactSFX;
-    [SerializeField]private GameObject pooledAudioPrefab;
+    [SerializeField] private AudioClip[] bulletImpactSFX;
+
+    [SerializeField] private AudioClip[] bulletImpactToWoodSFX;
+    [SerializeField] private AudioClip[] bulletImpactToMetalSFX;
+    [SerializeField] private AudioClip[] bulletImpactToSandSFX;
+    [SerializeField] private GameObject pooledAudioPrefab;
 
     private void OnEnable()
     {
@@ -13,10 +18,30 @@ public class BulletImpactSound : MonoBehaviour
 
     private void PlayRandomImpactSound()
     {
-        if (bulletImpactSFX == null || bulletImpactSFX.Length == 0) return;
+        Transform parent = transform.parent;
+        AudioClip[] sfxArray = bulletImpactSFX;
 
-        int randomIndex = Random.Range(0, bulletImpactSFX.Length);
-        SoundManager.Instance.PlaySFXForClip(bulletImpactSFX[randomIndex], gameObject.transform.position);
+        if (parent != null)
+        {
+            if (parent.CompareTag("Sand"))
+            {
+                sfxArray = bulletImpactToSandSFX;
+            }
+            else if (parent.CompareTag("Target") || parent.CompareTag("Metal"))
+            {
+                sfxArray = bulletImpactToMetalSFX;
+            }
+            else if (parent.CompareTag("Wood"))
+            {
+                sfxArray = bulletImpactToWoodSFX;
+
+            }
+        }
+
+        if (sfxArray == null || sfxArray.Length == 0) return;
+
+        int randomIndex = Random.Range(0, sfxArray.Length);
+        SoundManager.Instance.PlaySFXForClip(sfxArray[randomIndex], gameObject.transform.position);
 
     }
 }
