62fef4cda67e40b32d10477ea43f5649e7c74f1a
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-05-20 21:00:33 +0900

    [Fix] 로비씬에서 전환 안되는 버그 수정
    CoroutineRunner.cs
    - 유틸리티 추가로 Monobehavior 아닌 클래스에서도 코루틴 사용 가능
    
    StageScene.cs
    -EnterScene() 메서드를 순차적으로 진행

--- Assets/00_Scripts/Scene/StageScene.cs
+++ Assets/00_Scripts/Scene/StageScene.cs
@@ -1,17 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class StageScene : BaseScene
 {
     public override void EnterScene()
     {
-        UIManager.ToggleMouseCursor(false);
-        RoomManager.Instance.InitRoom();
-        StageManager.Instance.SpawnPlayer();
-        UIManager.Instance.InitMainUI<HUDUI>();
-        var bgm = ResourceManager.Instance.Load<AudioClip>("Audio/BGM/Can't Stop Me_demo");
-        SoundManager.Instance.PlayBackgroundMusic(bgm);
+        CoroutineRunner.Run(EnterSceneRoutine());
     }
 
+    private IEnumerator EnterSceneRoutine()
+    {
+        UIManager.ToggleMouseCursor(false);
+        yield return null;
+
+        RoomManager.Instance.InitRoom();
+        yield return null;
+
+        StageManager.Instance.SpawnPlayer();
+        yield return null;
+
+        UIManager.Instance.InitMainUI<HUDUI>();
+        yield return null;
+
+        var bgm = ResourceManager.Instance.Load<AudioClip>("Audio/BGM/Can't Stop Me_demo");
+        yield return null;
+
+        SoundManager.Instance.PlayBackgroundMusic(bgm);
+        yield return null;
+    }
     public override void ExitScene()
     {
         base.ExitScene();
