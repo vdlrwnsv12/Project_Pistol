b295a1518972dcc1dc46ea17d80f0de500f10669
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-05-21 10:05:01 +0900

    [Fix] loadingScene 출력 안되는 문제 해결

--- Assets/00_Scripts/UI/Main/StartScene/LoadingLogoFiller.cs
+++ Assets/00_Scripts/UI/Main/StartScene/LoadingLogoFiller.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
-using DataDeclaration;
+using DataDeclaration;// Scene enum 정의 위치
 using System;
 using UnityEngine.SceneManagement;
-using System.Threading; // Scene enum 정의 위치
 
 /// <summary>
 /// 흐린 배경 위에 좌측부터 점점 뚜렷해지는 로딩 로고 연출 → 완료 시 다음 씬 전환
