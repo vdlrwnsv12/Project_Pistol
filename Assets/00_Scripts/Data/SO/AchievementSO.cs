feb00c214bc53cad20972c2fe91b25c4c354a506
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-05-22 15:01:35 +0900

    [Resource] AchivementSO추가

---
+++ Assets/00_Scripts/Data/SO/AchievementSO.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections.Generic;
+[CreateAssetMenu(fileName = "AchievementSO", menuName = "SO/AchievementSO")]
+public class AchievementSO : ScriptableObject
+{
+    public string ID;
+    public string Name;
+    public string Description;
+}
+
+[System.Serializable]
+public class AchievementData
+{
+    public string ID;
+    public string Name;
+    public string Description;
+}
