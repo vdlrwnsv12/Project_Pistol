d12ea91ff0b2e6323390ea87b735fd59f3949060
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-05-22 21:06:08 +0900

    [Feat] 도전과제 생성 기능 추가(미구현)

---
+++ Assets/AchivementDataContainer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AchivementDataContainer : MonoBehaviour
+{
+    public TextMeshProUGUI achiveText;
+    public TextMeshProUGUI titleText;
+}
