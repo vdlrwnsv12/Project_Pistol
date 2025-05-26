bcee18d6e769c512dcbb0e10a1462202593ad99e
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-05-26 12:07:19 +0900

    [Feat] 리더보드 출력 기능

--- Assets/00_Scripts/UI/Main/LobbyUI.cs
+++ Assets/00_Scripts/UI/Main/LobbyUI.cs
@@ -1,22 +1,17 @@
 using System;
 using DataDeclaration;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LobbyUI : MainUI
 {
     public override MainUIType UIType { get; protected set; }
 
-    private string characterText1;
-    private string characterText2;
-    private string characterText3;
-
-    [SerializeField] private Button optionButton;//테스트
+    [SerializeField] private Button optionButton;
     [SerializeField] private Button achievementButton;
-    private bool isAchieve = false;
+    [SerializeField] private Button leaderBoardButton;
 
+    private bool isPopupActive = false;
 
     private void Awake()
     {
@@ -25,48 +20,32 @@ public class LobbyUI : MainUI
 
     private void Start()
     {
-        optionButton.GetComponent<Button>().onClick.AddListener(OnClickToggleOptionUI);
-        achievementButton.GetComponent<Button>().onClick.AddListener(OnClickToggleAchievementUI);
+        optionButton.onClick.AddListener(OnClickToggleOptionUI);
+        achievementButton.onClick.AddListener(OnClickToggleAchievementUI);
+        leaderBoardButton.onClick.AddListener(OnClickToggleLeaderBoardUI);
     }
 
-    private void OnClickToggleOptionUI()
+    private void TogglePopupUI<T>() where T : PopupUI
     {
-        if (isAchieve == true)
+        if (isPopupActive)
         {
             UIManager.Instance.ClosePopUpUI();
-            isAchieve = false;
+            isPopupActive = false;
         }
         else
         {
             UIManager.Instance.ClosePopUpUI();
-            UIManager.Instance.OpenPopupUI<PopupOption>();
-            isAchieve = true;
-        }
-
-    }
-
-    private void OnClickToggleAchievementUI()
-    {
-        if (isAchieve == true)
-        {
-            UIManager.Instance.ClosePopUpUI();
-            isAchieve = false;
-        }
-        else
-        {
-            UIManager.Instance.ClosePopUpUI();
-            UIManager.Instance.OpenPopupUI<PopupAchievementView>();
-            isAchieve = true;
+            UIManager.Instance.OpenPopupUI<T>();
+            isPopupActive = true;
         }
     }
+
+    private void OnClickToggleOptionUI() => TogglePopupUI<PopupOption>();
+    private void OnClickToggleAchievementUI() => TogglePopupUI<PopupAchievementView>();
+    private void OnClickToggleLeaderBoardUI() => TogglePopupUI<PopupLeaderBoard>();
 
     public override void SetActiveUI(MainUIType activeUIType)
     {
         gameObject.SetActive(UIType == activeUIType);
-    }
-
-    private void OnClickStartButton()
-    {
-
     }
 }
